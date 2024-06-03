220eca23e7629b058193401d99d6cb796e42f23a
hebertsanto <hebertsantos0704@gmail.com>
2024-06-03 09:54:39 -0300

    feat : create a decorator for public routes

--- src/modules/auth/controller/auth-controller.ts
+++ src/modules/auth/controller/auth-controller.ts
@@ -2,11 +2,20 @@ import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { AuthDTO } from '../auth.dto';
 import { Response } from 'express';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SkipAuth } from '../skip-auth.decorator';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiResponse({ status: HttpStatus.OK, description: 'User log in ' })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Credential are invalid',
+  })
+  @SkipAuth()
   @Post('login')
   public async signIn(@Body() signInDto: AuthDTO, @Res() res: Response) {
     const { access_token } = await this.authService.signIn(
--- src/modules/users/controllers/users-controllers.ts
+++ src/modules/users/controllers/users-controllers.ts
@@ -2,16 +2,24 @@ import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserDTO } from '../user-dto';
 import { UserService } from '../services/users.service';
 import { Response } from 'express';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SkipAuth } from 'src/modules/auth/skip-auth.decorator';
 
+@ApiTags('Users')
 @Controller('/users')
 export class UsersController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Create a new user in database',
+  })
+  @SkipAuth()
   @Post()
   public async create(@Body() usersDto: UserDTO, @Res() res: Response) {
     const user = await this.userService.create(usersDto);
     return res
       .status(HttpStatus.CREATED)
-      .json({ message: 'User created !', user });
+      .json({ message: 'User created', user });
   }
 }
